29affa15ebb55a99de75385e6673ab13f2678f3b
Emiliano Pérez <emi.esp86@gmail.com>
2022-06-13 12:53:51 -0300

    React Calculator

---
+++ src/components/Pantalla.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../stylesheets/Pantalla.css';
+
+const Pantalla = ({ input }) => {
+  return <div className='input'>{input}</div>;
+};
+
+export default Pantalla;
